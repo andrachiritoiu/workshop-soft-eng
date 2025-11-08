ecf5fd1c63ecf329e07358d9c87f51ce9a80d7c3
andrachiritoiu <chiritoiu.andra@yahoo.com>
2025-11-08 14:14:17 +0200

    adaugare functionalitati

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,6 +11,11 @@ class GildedRose(object):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
+
+                        # Degradare suplimentară pentru "Conjured" (prima degradare)
+                        if "Conjured" in item.name:
+                            if item.quality > 0:
+                                item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -29,6 +34,11 @@ class GildedRose(object):
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
+
+                                # Degradare suplimentară pentru "Conjured" (a doua degradare - după SellIn)
+                                if "Conjured" in item.name:
+                                    if item.quality > 0:
+                                        item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
